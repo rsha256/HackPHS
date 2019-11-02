ae14352768f5250e3fac1b2d7805984ab199bffd
Rahul Shah <rahuldshah3@gmail.com>
2019-11-02 17:49:35 -0400

    crèpes

--- recycleML/recycleML/__init__.py
+++ recycleML/recycleML/__init__.py
@@ -45,6 +45,6 @@ def create_app(test_config=None):
     # in another app, you might define a separate main index here with
     # app.route, while giving the blog blueprint a url_prefix, but for
     # the tutorial the blog will be the main index
-    app.add_url_rule("/", endpoint="index")
+    app.add_url_rule("/", endpoint="upload")
 
     return app
