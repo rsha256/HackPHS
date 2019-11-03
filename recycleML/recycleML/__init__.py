3e317491d7a3d1dda9cd144786c1c79357423706
Rahul Shah <rahuldshah3@gmail.com>
2019-11-03 05:08:00 -0500

    file upload pls work

--- recycleML/recycleML/__init__.py
+++ recycleML/recycleML/__init__.py
@@ -1,8 +1,44 @@
 import os
 
-from flask import Flask, render_template
+from flask import Flask, flash, request, redirect, url_for, render_template, send_from_directory
+from werkzeug.utils import secure_filename
+from datetime import datetime
+
+UPLOAD_FOLDER = 'static/img/tmp'
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
+
 
 app = Flask(__name__)
+
+# upload
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit an empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            now = datetime.now()
+            filename = now
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('uploaded_file',
+                                    filename=filename))
+    return render_template('index.html', image=send_from_directory(app.config['UPLOAD_FOLDER'],
+                               filename), title='Upload')
 
 
 @app.route('/')
