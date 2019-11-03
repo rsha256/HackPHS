4d8fb7566fcabd03babe315f790c50fd012977ea
Varun-S-Nair <varunsnair2002@gmail.com>
2019-11-02 20:41:54 -0400

    Edited Tester File

--- backend/tester.py
+++ backend/tester.py
@@ -1,16 +1,32 @@
 from fastai.vision import *
 from fastai.metrics import error_rate
 from pathlib import Path
-from glob2 import glob
-from sklearn.metrics import confusion_matrix
 import os
 from PIL import Image
+import warnings
+warnings.filterwarnings("ignore")
 
+def get_classification(image_name):
+    path = Path(os.getcwd())/"data"
+    tfms = get_transforms(do_flip=True,flip_vert=True)
+    data = ImageDataBunch.from_folder(path,test="test",ds_tfms=tfms,bs=16)
+    learn = create_cnn(data,models.resnet34,metrics=error_rate)
+    learn.load("trained_model")
 
-path = Path(os.getcwd())/"data"
-tfms = get_transforms(do_flip=True,flip_vert=True)
-data = ImageDataBunch.from_folder(path,test="test",ds_tfms=tfms,bs=16)
-learn = create_cnn(data,models.resnet34,metrics=error_rate)
-learn.load("trained_model")
+    prediction = learn.predict(open_image(image_name))
+    a = []
+    for i in prediction[2]:
+        a.append(i.item())
 
-print(learn.predict(open_image('plastic100.jpg')))
+    print(str(prediction[0]))
+    print(str((max(a))*100)[:5]+"%")
+
+    new = list(a)
+    new.remove(max(a))
+
+    classifications = ['cardboard', 'glass', 'metal', 'paper', 'plastic', 'trash']
+    print(classifications[a.index(max(new))])
+    print(str(max(new)*100)[:5]+"%")
+    print(open_image(image_name))
+
+get_classification('plastic100.jpg')
