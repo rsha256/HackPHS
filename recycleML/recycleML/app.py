df767d2d7fa14efa0880d4ab0d153ff05791a67a
Rahul Shah <rahuldshah3@gmail.com>
2019-11-03 04:40:20 -0500

    which heroku

--- recycleML/recycleML/app.py
+++ recycleML/recycleML/app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/index.html')
 def index():
-    return render_template('index.html', title='Upload')
+    return render_template('index.html', title='app')
 
 if __name__ == '__main__':
     app.run(debug=True)
